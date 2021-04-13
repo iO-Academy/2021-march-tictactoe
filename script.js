2f4db1848a298d8ccdfc459c6cc492afaadaecf2
Imy Pullan <imypullan@gmail.com>
2021-04-13 12:06:55 +0100

    let's do functions

--- script.js
+++ script.js
@@ -7,19 +7,22 @@ const playerMarker = 'x';
 const tiles = document.querySelectorAll('.tile');
 gameState = ["", "", "", "", "", "", "", "", ""];
 
-console.log(grid);
+console.log(tiles);
 
 tiles.forEach(function (tile) {
     tile.addEventListener('click', () => {
+        console.log(tile);
         //assigns player marker based on currentPlayer value
         decidePlayer(currentPlayer, playerMarker);
         //gets index value of tile from data attribute
         let clicked = tile.dataset.index;
+        console.log(clicked);
         //as long as tile has not been clicked,
         //will add mark to gameState array
         //and increment playerScore for next round
         if(gameState[clicked] == "") {
             alterGameState();
+            addToScreen();
             currentPlayer++;
         }
 
@@ -28,16 +31,19 @@ tiles.forEach(function (tile) {
 
 
 // beginning of decidePlayer
-if (currentPlayer % 2 == 0) {
-    playerMarker = 'o'
-} else {
-    playerMarker = 'x'
-}
+// if (currentPlayer % 2 == 0) {
+//     playerMarker = 'o'
+// } else {
+//     playerMarker = 'x'
+// }
 
+function addToScreen() {
+    tile.innerHTML = playerMarker;
+}
 
 //draft for alterGameState
-function alterGameState() {
-    let clicked = data - index.getAttribute
-    //push marker to indexed item in array
-    gameState.splice(clicked, 0, playerMarker)
-}
+// function alterGameState() {
+//     let clicked = data - index.getAttribute
+//     //push marker to indexed item in array
+//     gameState.splice(clicked, 0, playerMarker)
+// }
