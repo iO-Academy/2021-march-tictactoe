6da340b011a2c400d4df6ddd732ee21483662973
Isabelle Cochrane <isabelle_cochrane@hotmail.co.uk>
2021-04-14 10:18:19 +0100

    notYetClicked

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function playGame() {
     let currentPlayer = 'x';
     const tiles = document.querySelectorAll('.tile');
     //this is a boolean to regulate whether a player can click a tile.
-    let notClicked;
+    let notYetClicked;
     startBtn.removeEventListener('click', playGame, false);
     startBtn.style.opacity = 0.5;
     tiles.forEach(function (tile) {
@@ -36,11 +36,11 @@ function playGame() {
             //gets index value of tile from data attribute
             let clicked = tile.dataset.index;
             //assigns player marker based on currentPlayer value
-            let notClicked = alterGameState(gameState, clicked, currentPlayer);
+            let notYetClicked = alterGameState(gameState, clicked, currentPlayer);
             //as long as tile has not been clicked,
             //will add mark to gameState array
             //and increment playerScore for next round
-            if(notClicked) {
+            if(notYetClicked) {
                 addToScreen(tile, currentPlayer);
                 currentPlayer = decideTurn(currentPlayer);
             }
