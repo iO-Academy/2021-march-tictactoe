9d9112ed65de3e455eb93157791fc7ff1a20f374
Nathaniel Payne <somersetnathaniel@gmail.com>
2021-04-14 11:54:44 +0100

    added victory check and returned winner as string

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let startBtn = document.querySelector("#startButton");
 startBtn.addEventListener("click", playGame);
+
 //ash says we can use this as a global object
 let gameState = ["", "", "", "", "", "", "", "", ""];
+let currentPlayer = 'x';
 
 function decideTurn(currentPlayer) {
     if (currentPlayer === 'o') {
@@ -9,40 +11,95 @@ function decideTurn(currentPlayer) {
     }
     return 'o';
 }
+
 function addToScreen(tile, currentPlayer) {
-    tile.innerHTML = currentPlayer;
+    if (currentPlayer === 'x') {
+        tile.classList.add('clickedX')
+    }
+    if (currentPlayer === 'o') {
+        tile.classList.add('clickedO')
+    }
 }
 
 function alterGameState(gameState, clicked, currentPlayer) {
     if(gameState[clicked] === "") {
         gameState[clicked] = currentPlayer;
         return true;
-     }
+    }
     return false;
 }
 
+function clickEvent(event) {
+    let clicked = event.target.dataset.index;
+
+    let notYetClicked = alterGameState(gameState, clicked, currentPlayer);
+
+    if(notYetClicked) {
+        addToScreen(event.target, currentPlayer);
+        currentPlayer = decideTurn(currentPlayer);
+    }
+
+    event.target.removeEventListener('click', clickEvent)
+
+    let winner = checkWinner(gameState)
+
+    if (winner) {
+        console.log(winner)
+        // display win message/modal
+        startBtn.addEventListener("click", playGame);
+    }
+
+}
+
+function checkWinner(gameState) {
+
+    let winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    let winMessage;
+
+    winningCombinations.forEach((set) => {
+        let indexA = set[0];
+        let indexB = set[1];
+        let indexC = set[2];
+
+
+        if (
+            gameState[indexA] === "x" &&
+            gameState[indexB] === "x" &&
+            gameState[indexC] === "x"
+        ) {
+            winMessage = "Player 1 wins!"
+        }
+
+        if (
+            gameState[indexA] === "o" &&
+            gameState[indexB] === "o" &&
+            gameState[indexC] === "o"
+        ) {
+            winMessage = "Player 2 wins!"
+        }
+    })
+
+    return winMessage
+}
+
 function playGame() {
-    // this determines turn order
-    // this variable holds alternating player symbols
-    let currentPlayer = 'x';
+
     const tiles = document.querySelectorAll('.tile');
+
     startBtn.removeEventListener('click', playGame, false);
     startBtn.style.opacity = 0.5;
-    tiles.forEach(function (tile) {
-        tile.addEventListener('click', () => {
-            //gets index value of tile from data attribute
-            let clicked = tile.dataset.index;
-            //assigns player marker based on currentPlayer value
-            //this is a boolean to regulate whether a player can click a tile.
-            let notYetClicked = alterGameState(gameState, clicked, currentPlayer);
-            //as long as tile has not been clicked,
-            //will add mark to gameState array
-            //and increment playerScore for next round
-            if(notYetClicked) {
-                addToScreen(tile, currentPlayer);
-                currentPlayer = decideTurn(currentPlayer);
-            }
-        })
 
+    tiles.forEach(function (tile) {
+        tile.addEventListener('click', clickEvent)
     })
 }
